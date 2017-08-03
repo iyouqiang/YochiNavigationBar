d972995efe11c98fe898eb4a32278c4fa516c6a4
iyouqiang <iyouqiang@icloud.com>
2017-08-03 18:12:59 +0800

    加入验证项目

---
+++ YochiNavigationProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YochiNavigationProject
+//
+//  Created by Yochi·Kung on 2017/8/3.
+//  Copyright © 2017年 gongyouqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YochiNavigationProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YochiNavigationProject
+//
+//  Created by Yochi·Kung on 2017/8/3.
+//  Copyright © 2017年 gongyouqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
